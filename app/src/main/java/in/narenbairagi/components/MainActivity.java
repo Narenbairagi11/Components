07656d0fbb535cdeccbc1677914ca049227eb991
Naren Bairagi <narendrakumarvaishnv@gmail.com>
2024-10-04 21:30:50 +0530

    ?

---
+++ app/src/main/java/in/narenbairagi/components/MainActivity.java
@@ -0,0 +1,19 @@
+package in.narenbairagi.components;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
